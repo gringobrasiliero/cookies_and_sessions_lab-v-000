b00b6081360f810c34be3256a7987e01136b9eef
Nolan Byrnes <nolanehs@gmail.com>
2018-03-15 11:31:50 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,8 @@
 class ProductsController < ApplicationController
 
 def add
-@product = Products.find(params[:id])
-cart << @product.id
+
+cart << params[:product]
 redirect_to '/products/index'
 end
 
